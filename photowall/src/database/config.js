6c0af63368a07bbca5ad65457cd0dd513a5ffd37
denisvr <denisvr01@gmail.com>
2020-03-24 23:19:43 +0000

    Application finished

--- photowall/src/database/config.js
+++ photowall/src/database/config.js
@@ -1,14 +1,8 @@
 import * as firebase from 'firebase'
 
-
+//Your configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyB1CB92jb9GdeJCVq4-74La_QgrbdSV3EE",
-  authDomain: "photowall-5e963.firebaseapp.com",
-  databaseURL: "https://photowall-5e963.firebaseio.com",
-  projectId: "photowall-5e963",
-  storageBucket: "photowall-5e963.appspot.com",
-  messagingSenderId: "410794667149",
-  appId: "1:410794667149:web:9b44939146a5d6c1ab06c4"
+  
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
